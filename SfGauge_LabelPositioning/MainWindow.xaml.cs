c4821c0a3ff61629cd2c399b1667b37af068b2c6
Hemalatha <hemalathavinai@gmail.com>
2020-10-27 16:32:44 +0530

    Sample included

---
+++ SfGauge_LabelPositioning/MainWindow.xaml.cs
@@ -0,0 +1,40 @@
+﻿using Syncfusion.UI.Xaml.Gauges;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace SfGauge_LabelPositioning
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+
+    public class CircularScaleExt : CircularScale
+    {
+        public override void OnApplyTemplate()
+        {
+            base.OnApplyTemplate();
+            var panel = GetTemplateChild("LabelsPanel") as CircularPanel;
+            if (panel != null)
+                panel.Margin = new Thickness(0, 20, 0, 0);
+        }
+    }
+}
